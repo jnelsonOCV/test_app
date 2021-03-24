ce9cb350e1d67f4ff612c93425b4edf9e6f0ad0b
Josh Nelson <jnelson@myocv.com>
2021-03-24 16:40:24 -0500

    Round corners of delete drawable if a todo item (to match)

--- TodoTestApp/app/src/main/java/com/nelson/branch_app/SwipeToDeleteCallback.kt
+++ TodoTestApp/app/src/main/java/com/nelson/branch_app/SwipeToDeleteCallback.kt
@@ -2,8 +2,8 @@ package com.nelson.branch_app
 
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -15,7 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 class SwipeToDeleteCallback(private val adapter: ToDoAdapter) :
         ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
-    private val background: ColorDrawable = ColorDrawable(Color.RED)
+    private val background: GradientDrawable = GradientDrawable(GradientDrawable.Orientation.TL_BR, intArrayOf(Color.RED, Color.RED))
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
         return false
@@ -24,8 +24,8 @@ class SwipeToDeleteCallback(private val adapter: ToDoAdapter) :
     override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
         val itemView = viewHolder.itemView
-        val backgroundCornerOffset = if (viewHolder.itemViewType == 0) 0 else 8.toPX()
         val iconRes = if (viewHolder.itemViewType == 0) R.drawable.ic_delete_list else R.drawable.ic_delete
+        val radius = if (viewHolder.itemViewType == 0) 0f else 8.toPX().toFloat()
         val icon: Drawable = ContextCompat.getDrawable(adapter.context, iconRes)!!
 
         val iconMargin = (itemView.height - icon.intrinsicHeight) / 2
@@ -44,7 +44,7 @@ class SwipeToDeleteCallback(private val adapter: ToDoAdapter) :
                 val iconLeft = itemView.right - iconMargin - icon.intrinsicWidth
                 val iconRight = itemView.right - iconMargin
                 icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
-                background.setBounds(itemView.right + dX.toInt() - backgroundCornerOffset,
+                background.setBounds(itemView.right + dX.toInt(),
                         itemView.top, itemView.right, itemView.bottom)
             }
             else -> {
@@ -52,7 +52,7 @@ class SwipeToDeleteCallback(private val adapter: ToDoAdapter) :
                 background.setBounds(0, 0, 0, 0)
             }
         }
-
+        background.cornerRadii = floatArrayOf(0f, 0f, radius, radius, radius, radius, 0f, 0f)
         background.draw(c)
         icon.draw(c)
     }
